8b1e9cf150d08f1482e0fa8dfab73403bff3447a
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-10 15:56:49 +0200

    add test with strlen, str-shuffle, str-replace in function.php

---
+++ function/function.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // strlen
+
+    $phrase = "Test pour avoir le nombre de caractères dans cette phrase" . '<br />';
+    echo $phrase;
+    $longueur = strlen($phrase);
+    echo 'Il y a ' . $longueur . ' caractères dans la phrase ci-dessus' . '<br />';
+
+    // str_replace
+
+    $result = str_replace('u', 'o', "flour" );
+    echo $result; // flour => floor
+
+    // str_shuffle
+    $dog = "chien";
+    $shuffleDog = str_shuffle($dog);
+    echo $shuffleDog; // mélange les lettres
+    ?>
+
+    
+</body>
+</html>
