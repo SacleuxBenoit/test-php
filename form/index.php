138be56496fbe9c523d4136b041dced6dab48461
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-11 14:00:44 +0200

    add a test with textarea for the forms

--- form/index.php
+++ form/index.php
@@ -4,14 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
+
     <form method="post" action="cible.php">
     <label>votre pseudo :
         <input type="text" name="pseudo"/>
     </label>
-        <input type="submit" value="Valider"/>
-    </form>
+       
+    <p>
+        <label>votre commentaire : <br/>
+            <textarea name="message" rows="8" cols="45">
+            </textarea>
+            <input type="submit" value="Valider"/>
+        </label>
+    </p>
+      </form>
 </body>
 </html>
