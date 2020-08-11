2d5f4435da9c86b2f27eabbb09a11761556209e6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-11 13:11:26 +0200

    add a paragraph, with  in cible.php

---
+++ form/cible.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>Bienvenue <?php echo $_POST['pseudo'] ?>
+    tu peut modifier ton pseudo en cliquant <a href="index.php">ici</a>
+    </p>
+</body>
+</html>
