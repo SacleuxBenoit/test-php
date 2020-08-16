6b46deba733afec79d3a3b7be56c80cd483f6a90
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-16 15:37:37 +0200

    add the form in quickChat.php

---
+++ quickChat/quickChat.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="post" action="quickChat_post.php">
+    <p>
+        <label for="username" name="username">Username
+            <input type="text" id="username" name="username">
+        </label>
+    </p>
+
+    <p>
+        <label for="message">Message
+            <input type="text" id="message">
+        </label>
+    </p>
+
+    <p>
+        <input type="submit" value="Envoyer">
+    </p>
+</form>
+</body>
+</html>
