cad653c118c13d2b9edf9aab4fc48e1c47019b22
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-18 09:35:36 +0200

    add content in prepared_Statement.php

---
+++ PreparedStatements/prepared_Statement.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = ?  AND prix <= ? ORDER BY prix');
+$req->execute(array($_GET['possesseur'], $_GET['prix_max']));
+
+echo '<ul>';
+while ($donnees = $req->fetch())
+{
+	echo '<li>' . $donnees['nom'] . ' (' . $donnees['prix'] . ' EUR)</li>';
+}
+echo '</ul>';
+
+$req->closeCursor();
+
+?>
+</html>
+</body>
