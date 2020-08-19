551b3dc9eee9fce9a9373de0bb6c57a0d50e6f75
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-19 21:03:22 +0200

    add the form in create.php

--- create_account/create.php
+++ create_account/create.php
@@ -10,6 +10,38 @@ session_start();
     <title>Document</title>
 </head>
 <body>
+
+    <div>
+        <form action="hello.php" method="post">
+            <p>
+                <label for="last_name">Last name</label>
+                <input type="text" name="last_name" id="last_name">
+            </p>
+
+            <p>
+                <label for="first_name">First name</label>
+                <input type="text" name="first_name" id="first_name">
+            </p>
+
+            <p>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email">
+            </p>
+
+            <p>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </p>
+
+                <input type="submit" value="Validate">
+
+        </form>
+    </div>
+
+
+
+
+
     <?php 
     try{
         $bdd = new PDO("mysql:host=localhost;dbname=testAccount;charset=utf8", "root", $_SESSION['pass']);
