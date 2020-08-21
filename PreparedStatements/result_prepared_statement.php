15895f1f788f49b7f02843cd3f9c52d980251f40
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-21 17:15:28 +0200

    add connection to the bdd + a prepared statement for add the result of the form in the database in result_prepared_statement.php

---
+++ PreparedStatements/result_prepared_statement.php
@@ -0,0 +1,36 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset:utf8', 'root', $_SESSION['pass']);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(Exception $e){
+    die('Erreur : '.$e->getMessage());
+}
+
+$inser = $bdd->prepare('INSERT INTO info (classe, race, pseudo) VALUES(:classe, :race, :pseudo)');
+$inser->bindParam(':classe', $_POST['classe']);
+$inser->bindParam(':race',  $_POST['race']);
+$inser->bindParam(':pseudo', $_POST['pseudo']);
+$inser->execute();
+
+
+echo "Vous venez d'enregistrer dans la base de données : " . '<br />' . "<strong>Classe</strong> : "
+    . htmlspecialchars($_POST['classe']) . '<br />' . "<strong>Race</strong> : "
+    . htmlspecialchars($_POST['race']) . '<br />' . "<strong>Pseudo</strong> : "
+    . htmlspecialchars($_POST['pseudo']) . '<br />'
+    ?>
+</body>
+</html>
