93026c16f0d2445b2244451b89197ebc3f0d096c
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-21 15:48:41 +0200

    add test about prepared statement in prepared_Statement.php

--- PreparedStatements/prepared_Statement.php
+++ PreparedStatements/prepared_Statement.php
@@ -1,36 +1,25 @@
 <?php
 session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+
+try{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', $_SESSION['pass']);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 }
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
+catch(Exception $e){
+    die('Erreur : '.$e->getMessage());
 }
 
-$req = $bdd->prepare('SELECT nom, prix FROM jeux_video WHERE possesseur = ?  AND prix <= ? ORDER BY prix');
-$req->execute(array($_GET['possesseur'], $_GET['prix_max']));
+$inser = $bdd->prepare('INSERT INTO info (classe, race, pseudo) VALUES(:classe, :race, :pseudo)');
+$inser->bindParam(':classe', $classe);
+$inser->bindParam(':race', $race);
+$inser->bindParam(':pseudo', $pseudo);
 
-echo '<ul>';
-while ($donnees = $req->fetch())
-{
-	echo '<li>' . $donnees['nom'] . ' (' . $donnees['prix'] . ' EUR)</li>';
-}
-echo '</ul>';
-
-$req->closeCursor();
+$classe = "hunter";
+$race = "Eds";
+$pseudo = "Totor";
+$inser->execute();
+echo "La requête a été envoyée";
 
 ?>
 </html>
