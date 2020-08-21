f1880286d9c0719944983929d674e76988aaf9e3
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-21 22:46:50 +0200

    hash the password in add_database.php

--- create_account/add_database.php
+++ create_account/add_database.php
@@ -10,11 +10,13 @@ catch(Exception $e){
     die('Erreur : '.$e->getMessage());
 }
 
+$pass_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
 $req_inser = $bdd->prepare('INSERT INTO Account (nom, prenom, email, pass) VALUES(:nom, :prenom, :email, :hash_pass)');
 $req_inser->bindParam(':nom', $_POST['last_name']);
 $req_inser->bindParam(':prenom', $_POST['first_name']);
 $req_inser->bindParam(':email', $_POST['email']);
-$req_inser->bindParam(':hash_pass', $_POST['password']);
+$req_inser->bindParam(':hash_pass', $pass_hash);
 $req_inser->execute();
 
 header('Location: hello.php');
