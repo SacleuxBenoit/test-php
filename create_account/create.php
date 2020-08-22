a597f7141ea88fb5936a08e9f4a7f40c5e202d68
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-22 19:34:59 +0200

    add a link to style.css in create.php

--- create_account/create.php
+++ create_account/create.php
@@ -8,6 +8,7 @@ include("hide.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
@@ -35,7 +36,7 @@ include("hide.php");
             </p>
 
                 <input type="submit" value="Validate">
-
+                
         </form>
     </div>
 
