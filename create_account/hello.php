d3ba537d73ba5c5cfee0e917631786f2e10bc1b6
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-22 19:36:35 +0200

    add a div, display (nom,prenom,email) and add a link for edit them in hello.php

--- create_account/hello.php
+++ create_account/hello.php
@@ -7,9 +7,16 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <p>Bonjour <?php echo $_SESSION['last_name'] ?> Votre compte vient d'être créée</p>
+    <div>
+        <p>Vous venez de vous inscrire avec les identifiants suivants :</p>
+        <p>Nom : <?php echo $_SESSION['last_name'] ?></p>
+        <p>Prénom : <?php echo $_SESSION['first_name'] ?></p>
+        <p>Email : <?php echo $_SESSION['email'] ?></p>
+        <p>Si vous voulez modifier vos informations merci de vous rendre sur <a href="edit_account.php">cette page</a></p>
+    </div>
 </body>
 </html>
