197eaab3005ce784d0c4a1ebfc2ec9def0fe6e26
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-24 14:58:47 +0200

    add the part for update the database in change_name.php

--- data_retrieval_from_database/change_name.php
+++ data_retrieval_from_database/change_name.php
@@ -13,5 +13,19 @@ session_start();
     <p>Vous venez de modifier vos informations pour : </p>
     <p>Name : <?php echo $_POST['edit_name'] ?></p>
     <p>Username : <?php echo $_POST['edit_username'] ?></p>
+
+    <?php
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', $_SESSION['pass']);
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
+
+        $update_bdd = $bdd->prepare('UPDATE recupData SET name=?, username=? WHERE ID = 26');
+        $update_bdd->execute([$_POST['edit_name'], $_POST['edit_username']]);
+    ?>
 </body>
 </html>
