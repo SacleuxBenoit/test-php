89b508290e95ec60e8eedd633dc8db548f47c9b5
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-26 16:53:14 +0200

    add test with ( |, ^ and $) in index.php

--- Regex/index.php
+++ Regex/index.php
@@ -40,5 +40,37 @@
     }
     ?>
 </p>
+
+        <!-- Rechercher un mot avec le symbole | -->
+<p>
+    <?php
+        if(preg_match("#World|Warcraft#i","C'est un test")){
+            echo "le mot 'World' ou 'warcraft' a était trouvé.";
+        }else{
+            echo "impossible de trouver 'World' ou 'Warcraft' ";
+    }
+    ?>
+
+            <!-- Rechercher un mot avec le symbole ^ -->
+<p>
+    <?php
+        if(preg_match("#^Bienvenue#i","Bienvenue dans World Of Warcraft")){
+            echo "Le premier mot commence par Bienvenue.";
+        }else{
+            echo "Le premier mot ne commence pas par Bienvenue.";
+    }
+    ?>
+</p>
+
+            <!-- Rechercher un mot avec le symbole $ -->
+<p>
+    <?php
+        if(preg_match("#Warcraft$#i","Bienvenue dans World Of Warcraft")){
+            echo "Le dernier mot est : Warcraft.";
+        }else{
+            echo "Le denier mot n'est pas : Warcraft.";
+    }
+    ?>
+</p>
 </body>
 </html>
