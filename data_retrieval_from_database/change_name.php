26a390f4fc61943f4de4a7bc157c6caabd130d60
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-27 18:32:06 +0200

    add setAttribute for track the errors in change_name.php and new_data.php

--- data_retrieval_from_database/change_name.php
+++ data_retrieval_from_database/change_name.php
@@ -18,6 +18,7 @@ session_start();
         try
         {
             $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', $_SESSION['pass']);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(Exception $e)
         {
