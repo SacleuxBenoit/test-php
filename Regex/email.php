290fcb12996a2ab51e9ae6377ca518df39b41d1b
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-08-29 10:50:02 +0200

    add the form for the email in email.php

--- Regex/email.php
+++ Regex/email.php
@@ -7,6 +7,20 @@
     <title>Document</title>
 </head>
 <body>
+    <form method="post">
+        <p>
+            <label for="email">Email :</label>
+            <input id="email" name="email">
+        </p>
+        
+        <p>
+            <input type="submit" value="Vérifier l'email">
+        </p>
+    </form>
+
+
+
+
     <?php
         if(preg_match("#^[a-z0-9.-_]+@[a-z0-9.-_]{2,}\.[a-z]{2,4}$#", "test@test.com")){
             echo "ok";
