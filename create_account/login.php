189d517582ca1710a3052f267523e1c1f1580404
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-03 21:51:46 +0200

    add action=database/connexion_database, method=post for the form + change the type text to email for the input email in login.php

--- create_account/login.php
+++ create_account/login.php
@@ -11,16 +11,17 @@ include('hide.php');
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Pour vous connecter, veuillez remplir le formulaire ci-dessous</h1>
 
-        <form>
+        <form action="./database/connexion_database.php" method="post">
             <p>
                 <label for="loginEmail">Email :</label>
-                <input type="text" name="loginEmail" id="loginEmail">
+                <input type="email" name="loginEmail" id="loginEmail">
             </p>
 
-            <p>
+           <p>
                 <label for="loginPassword">Password :</label>
                 <input type="password" name="loginPassword" id="loginPassword">
             </p>
