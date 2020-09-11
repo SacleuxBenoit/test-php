36a47a99fec663599beb17422c752c9c101a6c7e
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-11 08:39:27 +0200

    add the email address to setCookie in connexion_database.php

--- create_account/database/connexion_database.php
+++ create_account/database/connexion_database.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+setcookie('user_email', $_POST['loginEmail'], time() + 24 * 3600);
 
 try{
     $bdd = new PDO("mysql:host=localhost;dbname=testAccount;charset=utf8", "root", $_SESSION['pass']);
