b1bd9c0b98cdd2cd021e5f0faafed4f3ec43227d
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-09-27 15:48:48 +0200

    add the connection to the database in connection_database.php

---
+++ micro_blog/test_comment/connection_database.php
@@ -0,0 +1,11 @@
+<?php
+try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=comment_test;charset=utf8', 'root', $_SESSION['pass']);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+?>
