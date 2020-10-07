47765de5bf36e79dc555903edbbcaf221c16b121
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-07 22:35:05 +0200

    in index.php add the doctype + form with 1 input type text and 1 button and in connection_database.php change the name of the database

--- searchBar/database/connection_database.php
+++ searchBar/database/connection_database.php
@@ -2,7 +2,7 @@
 include('./pass.php');
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=randomQuotes;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
+            $bdd = new PDO('mysql:host=localhost;dbname=searchBar;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         catch(Exception $e)
