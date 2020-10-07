e4547161af9c6304cab9e5cd29d8f5ab2891e0fd
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-07 22:04:03 +0200

    add the connection to the database in the folder searchBar

---
+++ searchBar/database/connection_database.php
@@ -0,0 +1,12 @@
+<?php
+include('./pass.php');
+        try
+        {
+            $bdd = new PDO('mysql:host=localhost;dbname=randomQuotes;charset=utf8', $_SESSION['user'], $_SESSION['pass']);
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(Exception $e)
+        {
+                die('Erreur : '.$e->getMessage());
+        }
+?>
