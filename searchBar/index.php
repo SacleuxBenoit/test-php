236eb9eb868eed377d2599ac40365023decfcc27
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-08 13:31:26 +0200

    change the action for the form to index.php, add an id and name to the input, SELECT * in the table infos,  and display (jeux + console) in index.php

--- searchBar/index.php
+++ searchBar/index.php
@@ -6,11 +6,26 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="#" method="POST">
+    <form action="index.php" method="POST">
         <p>
-            <input type="text">
+            <input type="text" id="search" name="search">
             <button>Valider</button>
         </p>
     </form>
+
+    <?php
+
+        include('./database/connection_database.php');
+        
+        if(isset($_POST['search'])){
+            $searchBar = $bdd->prepare('SELECT * from infos WHERE jeux = :getGame');
+            $searchBar->bindParam(':getGame', $_POST['search']);
+            $searchBar->execute();
+    
+            $display = $searchBar->fetch();
+            
+            echo $display['jeux'] . " " . $display['console'];
+        }
+    ?>
 </body>
 </html>
