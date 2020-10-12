c9d8240c64044e993d8034e9ee715c117695e7bd
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-12 18:54:53 +0200

    add the folder Session, with a basic form in index.php

---
+++ cookieAndSession/Session/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="home.php" method="POST">
+        <p>
+            <label for="nom">Nom</label>
+            <input type="text" id="nom" name="nom">
+        </p>
+
+        <p>
+            <label for="nom">Nom</label>
+            <input type="text" id="nom" name="nom">
+        </p>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
