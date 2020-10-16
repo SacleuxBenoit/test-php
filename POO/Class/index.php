653f0e7554a950f1fede2ed73487778f9e368c57
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-16 16:29:55 +0200

    add a class with 4 attributs and 1 method in index.php of Class

---
+++ POO/Class/index.php
@@ -0,0 +1,12 @@
+<?php
+class Personnage{
+    private $_force = 30;
+    private $_localisation = "Azeroth";
+    private $_experience = 0;
+    private $_damage = 0;
+
+    public function damage(){
+        // Do something
+    }
+}
+?>
