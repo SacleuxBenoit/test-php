a71b86b1679bff6821fa62361dc46bc320c71860
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-16 23:15:17 +0200

    add public function displayExpe and getExpe in index.php of Class

--- POO/Class/index.php
+++ POO/Class/index.php
@@ -2,14 +2,24 @@
 class Personnage{
     private $_force = 30;
     private $_localisation = "Azeroth";
-    private $_experience = 0;
+    private $_experience = 1;
     private $_damage = 0;
 
     public function sayHi(){
         echo "Hello";
     }
+
+    public function displayExpe(){
+        echo $this->_experience;
+    }
+
+    public function getExpe(){
+        $this->_experience = $this->_experience + 1;
+    }
 }
 
 $perso = new Personnage;
 $perso->sayHi();
+$perso->getExpe();
+$perso->displayExpe();
 ?>
