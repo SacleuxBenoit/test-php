4c68328df7bdddf678771ef0dbf2a758ab6ef720
SacleuxBenoit <benoitsacleux@outlook.fr>
2020-10-19 12:22:45 +0200

    clear the page, add class Personnage, with 3 private attributes and 2 methods in POO/Class/index.php

--- POO/Class/index.php
+++ POO/Class/index.php
@@ -1,34 +1,15 @@
-<?php
+<?php 
 class Personnage{
-    private $_force = 30;
-    private $_localisation = "Azeroth";
-    private $_experience = 1;
-    private $_damage = 0;
+    private $_damage;
+    private $_exp;
+    private $strength;
 
-    public function sayHi(){
-        echo "Hello";
+    public function hit($persoToHit){
+        $persoToHit->_damage += $this->_force;
     }
 
-    public function displayExpe(){
-        echo $this->_experience;
+    public function winExp(){
+        $this->_exp = $this->_exp + 1;
     }
-
-    public function getExpe(){
-        $this->_experience = $this->_experience + 1;
-    }
-
-    public function looseExpe(){
-        $this->_experience = $this->_experience - 1;
-    }
-
 }
-
-$perso = new Personnage;
-$secondPerso = new Personnage;
-
-$perso->sayHi();
-$perso->displayExpe();
-$perso->looseExpe();
-$secondPerso->sayHi();
-$secondPerso->getExpe();
 ?>
