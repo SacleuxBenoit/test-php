8108fbe816741e2a4bb9d86a11a809b7b9170fa0
SacleuxBenoit <benoitsacleux@outlook.fr>
2021-12-28 15:16:24 +0100

    add doctype + form with input username && submit :arrow_right: index.php of regex

---
+++ Regex/username/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./create_username.php" method="post">
+        <label for="username">username</label>
+        <p>
+            <input type="text" name="username" id="username" placeholder="username...">
+        </p>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
